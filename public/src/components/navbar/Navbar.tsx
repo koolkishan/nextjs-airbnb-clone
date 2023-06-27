0bbe44af084a6f3e05f60a466054e421f2c4eae2
koolkishan <kishansheth21@gmail.com>
2023-06-28 00:28:17 +0530

    Fixed ScheduleBar position.

--- public/src/components/navbar/Navbar.tsx
+++ public/src/components/navbar/Navbar.tsx
@@ -53,39 +53,41 @@ function Navbar() {
   };
 
   return (
-    <header className="h-20 border-b border-b-gray-200  flex justify-between items-center px-20">
-      <AirBnbLogo />
+    <header className="h-20 border-b border-b-gray-200 relative">
       <ScheduleBar />
-      <ul className="flex items-center gap-6 font-medium">
-        <li className="cursor-pointer">
-          <span>Airbnb your home</span>
-        </li>
-        <li className="cursor-pointer">
-          <FiGlobe />
-        </li>
-        <li
-          className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-500"
-          onClick={(e) => showContextMenu(e)}
-        >
-          <RxHamburgerMenu className="text-lg" />
-          <span>
-            <Image
-              src="/empty-profile.png"
-              alt="profile"
-              height={30}
-              width={30}
-            />
-          </span>
-        </li>
-      </ul>
-      {isContextMenuVisible && (
-        <ContextMenu
-          options={contextMenuOptions}
-          cordinates={contextMenuCordinates}
-          contextMenu={isContextMenuVisible}
-          setContextMenu={setIsContextMenuVisible}
-        />
-      )}
+      <div className="h-full flex justify-between items-center px-20">
+        <AirBnbLogo />
+        <ul className="flex items-center gap-6 font-medium">
+          <li className="cursor-pointer">
+            <span>Airbnb your home</span>
+          </li>
+          <li className="cursor-pointer">
+            <FiGlobe />
+          </li>
+          <li
+            className="flex cursor-pointer items-center gap-2 border border-gray-300 py-2 px-3 rounded-full hover:shadow-xl transition-all duration-500"
+            onClick={(e) => showContextMenu(e)}
+          >
+            <RxHamburgerMenu className="text-lg" />
+            <span>
+              <Image
+                src="/empty-profile.png"
+                alt="profile"
+                height={30}
+                width={30}
+              />
+            </span>
+          </li>
+        </ul>
+        {isContextMenuVisible && (
+          <ContextMenu
+            options={contextMenuOptions}
+            cordinates={contextMenuCordinates}
+            contextMenu={isContextMenuVisible}
+            setContextMenu={setIsContextMenuVisible}
+          />
+        )}
+      </div>
     </header>
   );
 }
